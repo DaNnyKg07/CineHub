361e7cdf82877d326418e4fcb00d6da740078935
DaNnyKg07 <parasyadavnirban999@gmail.com>
2023-09-20 14:27:20 +0530

    initial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                At Movix, we are dedicated to providing you with an unparalleled entertainment experience. 
+                Our mission is to bring the world of movies and TV series to your fingertips, making it easier than ever for you to explore, discover, and enjoy your favorite content. 
+                 With a vast library of films and shows spanning various genres, we strive to cater to every taste and preference. 
+                 We are committed to delivering high-quality streaming, user-friendly navigation, and a platform that evolves with your needs. 
+                 Thank you for choosing Movix as your go-to destination for cinematic adventures.
+                </div>
+                <div className="socialIcons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
